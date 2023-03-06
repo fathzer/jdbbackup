07bba11bfe02b4d0302255c3b89e28a024efdc77
Fathzer <jm-dev@astesana.net>
2023-03-06 09:43:36 +0100

    Testing proxy in sftp

--- src/test/java/com/fathzer/jdbbackup/managers/sftp/SFTPManagerTest.java
+++ src/test/java/com/fathzer/jdbbackup/managers/sftp/SFTPManagerTest.java
@@ -10,10 +10,12 @@ import java.util.Vector;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 
+import com.fathzer.jdbbackup.utils.ProxySettings;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.ChannelSftp.LsEntry;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.ProxyHTTP;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.SftpATTRS;
 import com.jcraft.jsch.SftpException;
@@ -22,6 +24,7 @@ class SFTPManagerTest {
 	@Test
 	void basicTest() throws JSchException, IOException, SftpException {
 		SFTPManager manager = new SFTPManager();
+		SFTPDestination dest = manager.validate("user:pwd/path/nonexisting/filename",x->x);
 		
 		final Session session = mock(Session.class);
 		final ChannelSftp channel = mock(ChannelSftp.class);
@@ -32,8 +35,6 @@ class SFTPManagerTest {
 		try (MockedConstruction<JSch> mock = mockConstruction(JSch.class, (jsch , context) -> {
 			when(jsch.getSession("user","127.0.0.1",22)).thenReturn(session);
 		})) {
-			SFTPDestination dest = new SFTPDestination("user:pwd/path/nonexisting/filename",x->x);
-			
 			manager.send(null, 0, dest);
 			verify(session).setPassword("pwd");
 			verify(session, never()).setProxy(null);
@@ -50,7 +51,42 @@ class SFTPManagerTest {
 			verify(channel).disconnect();
 		}
 	}
-	
+
+	@Test
+	void withProxy() throws JSchException, IOException, SftpException {
+		final ProxySettings settings = ProxySettings.fromString("a:b@host:3128");
+		SFTPManager manager = new SFTPManager();
+		
+		try (MockedConstruction<ProxyHTTP> mock = mockConstruction(ProxyHTTP.class)) {
+			manager.setProxy(settings);
+			final ProxyHTTP proxy = mock.constructed().get(0);
+			verify(proxy).setUserPasswd("a", "b");
+
+			SFTPDestination dest = manager.validate("user:pwd//absolute/filename",x->x);
+			
+			final Session session = mock(Session.class);
+			final ChannelSftp channel = mock(ChannelSftp.class);
+			when(session.openChannel("sftp")).thenReturn(channel);
+			when(channel.ls("/")).thenReturn(new Vector<>());
+			try (MockedConstruction<JSch> mockJSch = mockConstruction(JSch.class, (jsch , context) -> {
+				when(jsch.getSession("user","127.0.0.1",22)).thenReturn(session);
+			})) {
+				manager.send(null, 0, dest);
+				verify(session).setPassword("pwd");
+				verify(session).setProxy(proxy);
+				verify(session).connect();
+				verify(session).disconnect();
+				
+				verify(channel).connect();
+				verify(channel).ls("/");
+				verify(channel).mkdir("/absolute");
+				verify(channel).cd("/absolute");
+				verify(channel).put((InputStream)null, "filename");
+				verify(channel).disconnect();
+			}
+		}
+	}
+
 	private LsEntry getLSEntry(String fileName, boolean isDir) {
 		final SftpATTRS attr = mock(SftpATTRS.class);
 		when(attr.isDir()).thenReturn(isDir);
