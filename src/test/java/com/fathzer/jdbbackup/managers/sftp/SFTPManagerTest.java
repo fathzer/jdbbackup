fffee0caf15d41c0fdf0f543184058dcf83e71e3
Fathzer <jm-dev@astesana.net>
2023-03-06 09:19:00 +0100

    Continue Mocking SFTP

--- src/test/java/com/fathzer/jdbbackup/managers/sftp/SFTPManagerTest.java
+++ src/test/java/com/fathzer/jdbbackup/managers/sftp/SFTPManagerTest.java
@@ -3,15 +3,19 @@ package com.fathzer.jdbbackup.managers.sftp;
 import static org.mockito.Mockito.*;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Vector;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 
 import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.ChannelSftp.LsEntry;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpATTRS;
 import com.jcraft.jsch.SftpException;
 
 class SFTPManagerTest {
@@ -22,11 +26,13 @@ class SFTPManagerTest {
 		final Session session = mock(Session.class);
 		final ChannelSftp channel = mock(ChannelSftp.class);
 		when(session.openChannel("sftp")).thenReturn(channel);
-		when(channel.ls("./")).thenReturn(new Vector<>());
+		final Vector<LsEntry> firstLevelFiles = new Vector<>(Arrays.asList(getLSEntry("aFile", false), getLSEntry("aFolder", true), getLSEntry("path", true)));
+		when(channel.ls("./")).thenReturn(firstLevelFiles);
+		when(channel.ls("./path/")).thenReturn(new Vector<>());
 		try (MockedConstruction<JSch> mock = mockConstruction(JSch.class, (jsch , context) -> {
 			when(jsch.getSession("user","127.0.0.1",22)).thenReturn(session);
 		})) {
-			SFTPDestination dest = new SFTPDestination("user:pwd/path/filename",x->x);
+			SFTPDestination dest = new SFTPDestination("user:pwd/path/nonexisting/filename",x->x);
 			
 			manager.send(null, 0, dest);
 			verify(session).setPassword("pwd");
@@ -36,8 +42,21 @@ class SFTPManagerTest {
 			
 			verify(channel).connect();
 			verify(channel).ls("./");
+			verify(channel, never()).mkdir("./path");
+			verify(channel).ls("./path/");
+			verify(channel).mkdir("./path/nonexisting");
+//			verify(channel).cd("./path/nonexisting"); // FIXME
+			verify(channel).put((InputStream)null, "filename");
 			verify(channel).disconnect();
-			verify(channel).mkdir("./path");
 		}
+	}
+	
+	private LsEntry getLSEntry(String fileName, boolean isDir) {
+		final SftpATTRS attr = mock(SftpATTRS.class);
+		when(attr.isDir()).thenReturn(isDir);
+		final LsEntry entry = mock(LsEntry.class);
+		when(entry.getAttrs()).thenReturn(attr);
+		when(entry.getFilename()).thenReturn(fileName);
+		return entry;
 	}
 }
